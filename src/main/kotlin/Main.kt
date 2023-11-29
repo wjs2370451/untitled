37c1a7cb02077e61dae44b226a6b2734072e49ba
wjs2370451 <wjs2370451@naver.com>
2023-11-29 17:21:04 +0900

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,4 +5,5 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
     println("Program arguments: ${args.joinToString()}")
+    println("Program arguments: ${args.joinToString()}")
 }
